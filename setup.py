7dbf438031b432d5095ac7d1c1cca3b1c6485ebd
Sandeep <sandeepy@ubuntusydl.(none)>
2017-05-29 15:45:52 -0700

    Bump the version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='dnspy',
-		version='1.1',
+		version='2.0',
 		description='Library for domain name manipulation',
 		author='Sandeep Yadav',
 		url='https://github.com/sandeepvaday/dnspy',
